17cf6025f377e0bf92694569884caba43a813abd
najmul009 <najmul.tk16@gmail.com>
2022-05-22 11:46:37 +0600

    navbar and banner done

---
+++ src/Pages/Home/Banner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import banner from '../../assets/banner.png'
+const Banner = () => {
+  return (
+    <div class="hero min-h-screen bg-base-200 ">
+  <div class="hero-content flex-col lg:flex-row p-0">
+    <img src={banner} class="lg:max-w-lg sm:max-w-sm   " alt=''/>
+    <div>
+      <h1 class="text-5xl font-bold">NEW PRODUCTS IN STOCKS</h1>
+      <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <button class="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
+  );
+};
+
+export default Banner;
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        experstheme: {
+          primary: "#FDB913",
+          secondary: "#19D3AE",
+          accent: "#3A4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+
+}
